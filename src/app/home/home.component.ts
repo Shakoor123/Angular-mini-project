a1aaef52d3a00749ff046ab574bd671c25137935
shakoor123 <shakoorchachu2001@gmail.com>
2022-11-02 23:32:34 +0530

    login error handled api fetching not working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { movies } from '../../assets/data.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private http: HttpClient) {}
+  movies: any;
   ngOnInit(): void {
+    // this.getMovies();
+    // this.http
+    //   .get<any>(
+    //     'https://api.themoviedb.org/3/trending/all/day?api_key=851a15d43d97fe53ada086503e4e13e1'
+    //   )
+    //   .subscribe((data) => {
+    //     this.movies = data;
+    //   });
+    // console.log(this.movies);
   }
-
+  // getMovies = async () => {
+  //   const res = await this.http.get<any>(
+  //     'https://api.themoviedb.org/3/trending/all/day?api_key=851a15d43d97fe53ada086503e4e13e1'
+  //   );
+  // };
 }
---
+++ src/assets/data.js
@@ -0,0 +1,62 @@
+export const moviesss = [
+  {
+    id: 1,
+    title: "Im Westen nichts Neues",
+    image: `/2IRjbi9cADuDMKmHdLK7LaqQDKA.jpg`,
+    rating: 7.912,
+  },
+  {
+    id: 2,
+    title: "See How They Run",
+    image: `/r3rpSAi2yukZwr9H2km0WKGODWo.jpg`,
+    rating: 6.645,
+  },
+  {
+    id: 3,
+    title: "The Takeover",
+    image: "/i6bqxo8o0yYDUUaQtkHaism8Bkr.jpg",
+    rating: 6.3,
+  },
+  {
+    id: 4,
+    title: "The Woman King",
+    image: "/438QXt1E3WJWb3PqNniK0tAE5c1.jpg",
+    rating: 7.4,
+  },
+  {
+    id: 5,
+    title: "Barbarian",
+    image: "/67kTU8DU5YZYrBybH4gppqptZK9.jpg",
+    rating: 7.04,
+  },
+  {
+    id: 6,
+    title: "Top Gun: Maverick",
+    image: "/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg",
+    rating: 8.3,
+  },
+  {
+    id: 7,
+    title: "Black Adam",
+    image: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+    rating: 6.912,
+  },
+  {
+    id: 8,
+    title: "The Good Nurse",
+    image: "/rV0xrSgkOQj2EpiG8n16VHHJeg3.jpg",
+    rating: 7.14,
+  },
+  {
+    id: 9,
+    title: "Smile",
+    image: "/olPXihyFeeNvnaD6IOBltgIV1FU.jpg",
+    rating: 6.7,
+  },
+  {
+    id: 10,
+    title: "Black Panther: Wakanda Forever",
+    image: "/yYrvN5WFeGYjJnRzhY0QXuo4Isw.jpg",
+    rating: 8.5,
+  },
+];
