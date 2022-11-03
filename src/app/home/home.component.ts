02be3052f88edbbb20cf8ee62f2ec876e609e093
shakoor123 <shakoorchachu2001@gmail.com>
2022-11-03 08:53:21 +0530

    data fetched and displayed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { movies } from '../../assets/data.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,20 +8,18 @@ import { movies } from '../../assets/data.js';
 export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {}
   movies: any;
+  path: string = 'https://image.tmdb.org/t/p/original';
   ngOnInit(): void {
-    // this.getMovies();
-    // this.http
-    //   .get<any>(
-    //     'https://api.themoviedb.org/3/trending/all/day?api_key=851a15d43d97fe53ada086503e4e13e1'
-    //   )
-    //   .subscribe((data) => {
-    //     this.movies = data;
-    //   });
-    // console.log(this.movies);
+    this.getMovies();
   }
-  // getMovies = async () => {
-  //   const res = await this.http.get<any>(
-  //     'https://api.themoviedb.org/3/trending/all/day?api_key=851a15d43d97fe53ada086503e4e13e1'
-  //   );
-  // };
+  getMovies() {
+    this.http
+      .get<any>(
+        'https://api.themoviedb.org/3/trending/all/day?api_key=851a15d43d97fe53ada086503e4e13e1'
+      )
+      .subscribe((data) => {
+        this.movies = data.results;
+        console.log(this.movies);
+      });
+  }
 }
